0eaeff2007afb1b13dac0de74d03323e1ad8a7fb
yonilhp <yonil.hinostroza.27@unsch.edu.pe>
2024-08-22 11:27:40 -0500

    Mucho mejor quedó el Card

--- src/js/component/CharacterCard.jsx
+++ src/js/component/CharacterCard.jsx
@@ -4,16 +4,24 @@ import starWarsImag from "../../img/sc1.png";
 
 const CharacterCard = ({ character, onViewMore }) => {
   return (
-    <Card className="mx-2" style={{ width: "200px", height: "300px" }}>
+    <Card
+      className="mx-2"
+      style={{ width: "250px", height: "350px", position: "relative" }}
+    >
       <div className="ratio ratio-4x3">
         <Card.Img
           src={starWarsImag}
           className="card-img-top"
-          style={{ height: "175px", objectFit: "cover" }}
+          style={{ height: "240px", objectFit: "cover" }}
         />
       </div>
-      <Card.Body className="d-flex flex-column">
-        <Card.Title className="text-center">{character.name}</Card.Title>
+      <Card.Body
+        className="d-flex flex-column"
+        style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}
+      >
+        <Card.Title className="text-center">
+          {character.name || "No Name Available"}
+        </Card.Title>
         <Button
           variant="primary"
           className="mt-auto"
